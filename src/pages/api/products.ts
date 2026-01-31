51058f5daf52ea8e1d932efd3a832022989e50f7
Adomoraes <adomoraes@gmail.com>
2026-01-31 07:17:24 -0300

    refactor(api): Carregar produtos do sistema de arquivos
    - Ler o arquivo database.json dinamicamente
    - Usar os módulos fs e path
    - Garantir que os dados mais recentes sejam sempre exibidos

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,8 +1,11 @@
-// pages/api/products.ts
-
-import { NextApiRequest, NextApiResponse } from "next"
-import products from "../../../database.json"
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.status(200).json(products)
+  const filePath = path.join(process.cwd(), "database.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const products = JSON.parse(jsonData);
+
+  res.status(200).json(products);
 }
--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -1,11 +1,15 @@
-// pages/api/products/[id].ts
-
-import { NextApiRequest, NextApiResponse } from "next"
-import products from "../../../../database.json"
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
+import { ProductType } from "../../../services/products";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { id } = req.query
+  const { id } = req.query;
 
-	const product = products.find((p) => p.id === Number(id))
-	res.status(200).json(product)
+  const filePath = path.join(process.cwd(), "database.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const products: ProductType[] = JSON.parse(jsonData);
+
+  const product = products.find((p) => p.id === Number(id));
+  res.status(200).json(product);
 }
